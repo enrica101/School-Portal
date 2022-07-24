faeb61a91cdc28ee272eaf3a6245f89e2b8ae501
Enrica Carpena <enricajulescarpena@gmail.com>
2022-07-25 07:57:21 +0800

    name field in register altered

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     // Create and Store New User
     public function store(Request $request){
         $formInputs = $request->validate([
-            'name' => ['required', 'min:3'],
+            'name' => 'required',
             'email' => ['required', 'email', Rule::unique('users','email')],
             'password' => ['required','confirmed','min:6']
         ]);
