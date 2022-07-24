4c402bc8fa07133f1c959293e88d63f4d2aaaa62
Enrica Carpena <enricajulescarpena@gmail.com>
2022-07-25 07:17:48 +0800

    changes in routing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     //Show Registration Form
     public function register(){
-        return view('/register');
+        return view('/users/register');
     }
 
     // Create and Store New User
@@ -44,7 +44,7 @@ class UserController extends Controller
     }
 
     public function login(){
-        return view('/login');
+        return view('/users/login');
     }
 
     // Authenticate User
